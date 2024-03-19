1d3544095dba5ce655aea156ce63b9b9d725844e
Cristian Dumbravanu <99369873+CristianDmbr@users.noreply.github.com>
2024-03-19 22:17:33 +0000

    Add files via upload

---
+++ databaseTesting.py
@@ -0,0 +1,82 @@
+import numpy as np
+import pandas as pd
+
+dataBase = pd.read_csv('Database/DatabaseTest.csv')
+pd.set_option('display.max_columns', None)
+
+####################################################################################
+# Adding new columns:
+
+# Calculate the average of SABER PRO'S 'QR_PRO', 'CR_PRO', 'CC_PRO', 'ENG_PRO', 'WC_PRO' metrics
+dataBase["Average_Score_SABER_PRO"] = dataBase[['QR_PRO', 'CR_PRO', 'CC_PRO', 'ENG_PRO', 'WC_PRO']].mean(axis=1)
+dataBase["Performance_SABER_PRO"] = np.select(
+    [
+        dataBase['Average_Score_SABER_PRO'] >= 90,
+        dataBase['Average_Score_SABER_PRO'] >= 80,
+        dataBase['Average_Score_SABER_PRO'] >= 70,
+        dataBase['Average_Score_SABER_PRO'] >= 60
+    ],
+    [
+        'Best',
+        'Very Good',
+        'Good',
+        'Pass'
+    ],
+    default='Fail'
+)
+
+# Calculate the average of Saber 11 competences
+dataBase["Average_Score_Saber11"] = dataBase[['MAT_S11', 'CR_S11', 'CC_S11', 'BIO_S11', 'ENG_S11']].mean(axis=1)
+dataBase["Performance_Saber11"] = np.select(
+    [
+        dataBase['Average_Score_Saber11'] >= 90,
+        dataBase['Average_Score_Saber11'] >= 80,
+        dataBase['Average_Score_Saber11'] >= 70,
+        dataBase['Average_Score_Saber11'] >= 60
+    ],
+    [
+        'Best',
+        'Very Good',
+        'Good',
+        'Pass'
+    ],
+    default='Fail'
+)
+
+print("Before : ")
+print(dataBase["SCHOOL_TYPE"].unique())
+
+####################################################################################
+# Replace EDU_FATHER and EDU_MOTHER records of “Not sure” and “0” and “Ninguno” (meaning None) with “None”
+dataBase["EDU_FATHER"].replace({"Not sure": "None", "0": "None", "Ninguno": "None"}, inplace=True)
+dataBase["EDU_MOTHER"].replace({"Not sure": "None", "0": "None", "Ninguno": "None"}, inplace=True)
+
+# Replace OOC_FATHER and OCC_MOTHER records of “0”, “Home” and “Retires” with "unemployed"
+dataBase["OCC_FATHER"].replace({"0": "Unemployed", "Home" : "Unemployed", "Retired": "Unemployed"}, inplace=True)
+dataBase["OCC_MOTHER"].replace({"0": "Unemployed", "Home" : "Unemployed", "Retired": "Unemployed"}, inplace=True)
+
+# Chnage “Esta clasificada en otro Level del SISBEN” into "It is classified in another SISBEN Level" from SISBEN
+dataBase["SISBEN"].replace({"Esta clasificada en otro Level del SISBEN" : "It is classified in another SISBEN Level"}, inplace=True)
+
+# In PEOPLE_HOUSE replace “Nueve” into “Nine” and “Once” into “Eleven”
+dataBase["PEOPLE_HOUSE"].replace({"Nueve" : "Nine", "Once" : "Eleven"},inplace=True)
+
+# Replace “No” and “O” to “0 hours per week”
+dataBase["JOB"].replace({"No":"0 hours per week","0":"0 hours per week"},inplace=True)
+
+####################################################################################
+# Remove unnecesary features 
+dataBase.drop(columns=['COD_S11'], inplace=True)
+dataBase.drop(columns=['COD_SPRO'], inplace=True)
+dataBase.drop(columns=['SCHOOL_NAME'],inplace=True)
+dataBase.drop(columns=['UNIVERSITY'], inplace=True)
+
+####################################################################################
+
+#print("Categorical Features")
+#print("Numerical Features")
+#print(dataBase.select_dtypes(exclude=np.number).describe())
+#print(dataBase.select_dtypes(include=np.number).describe())
+
+print("Uniques after")
+print(dataBase["ACADEMIC_PROGRAM"].unique())
